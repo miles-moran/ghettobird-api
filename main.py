8eaf40796ea9c398a8c3bb76fbd934d17c99ef5e
Miles <miles-moran@hotmail.com>
2020-11-09 16:54:19 -0600

    preparing to launch on heroku

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from ghettobird import fly
 import json
 from selenium import webdriver
 from pprint import pprint
+import os
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=["POST"])
 def main():
@@ -16,14 +16,14 @@ def main():
         options = routine["options"]
         if "browser" in options.keys():
             if options["browser"] == True:
+                chrome_options = webdriver.ChromeOptions()
+                chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
                 chromedriver_location = 'c:/chromedriver.exe'
-                options["browser"] = webdriver.Chrome(
-                    executable_path=chromedriver_location)
+                options["browser"] = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     result = fly(routine)
     if browser:
         browser.quit()
     return jsonify(result["results"])
-
 
 if __name__ == '__main__':
     app.run()
