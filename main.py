7192a2ce2660ed0cc86d99fb454b2fb69147b93c
Miles <miles-moran@hotmail.com>
2020-11-09 22:02:22 -0600

    modified chrome settings

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ def main():
             if options["browser"] == True:
                 chrome_options = webdriver.ChromeOptions()
                 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-                chromedriver_location = 'c:/chromedriver.exe'
-                options["browser"] = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+                chrome_options.add_argument("--headless")
+                chrome_options.add_argument("--disable-dev-shm-usage")
+                chrome_options.add_argument("--no-sandbox")
+                browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+                options["browser"] = browser
     result = fly(routine)
     if browser:
         browser.quit()
